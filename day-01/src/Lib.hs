e52a61bd9e64816266210c8c105935e3a0ecf6b8
Seth Etter <mail@sethetter.com>
2019-12-01 21:11:12 -0600

    Day 1, part 2

--- day-01/src/Lib.hs
+++ day-01/src/Lib.hs
@@ -2,7 +2,14 @@
 module Lib where
 
 fuelNeeded :: [String] -> Int
-fuelNeeded = sum . map fuelFromMass . map (\w -> read w :: Int)
+fuelNeeded = sum . map (addRemainingFuel . fuelFromMass) . map (\w -> read w :: Int)
 
 fuelFromMass :: Int -> Int
 fuelFromMass x = (x `div` 3) - 2
+
+addRemainingFuel :: Int -> Int
+addRemainingFuel x = sum $ go [x] x
+  where go :: [Int] -> Int -> [Int]
+        go xs x'
+          | fuelFromMass x' > 0 = go (xs ++ [fuelFromMass x']) $ fuelFromMass x'
+          | otherwise = xs
--- day-01/test/Spec.hs
+++ day-01/test/Spec.hs
@@ -5,7 +5,13 @@ import Lib
 
 run :: IO ()
 run = hspec $ do
+
   describe "fuelFromMass" $ do
     it "calculates fuel from mass" $ do
       fuelFromMass 1969 `shouldBe` 654
       fuelFromMass 100756 `shouldBe` 33583
+
+  describe "addRemainingFuel" $ do
+    it "recursively adds the necessary remaining fuel" $ do
+      addRemainingFuel 2 `shouldBe` 2
+      addRemainingFuel 654 `shouldBe` 966
