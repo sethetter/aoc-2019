4be09a0a3a1b7485db5519ba8e1201a5bc7522f3
Seth Etter <mail@sethetter.com>
2019-12-06 21:52:58 -0600

    Day 6, part 2

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -2,17 +2,20 @@ use std::fs;
 use std::collections::HashMap;
 
 fn main() {
-    let raw_contents = fs::read_to_string("test_input.txt").expect("File not found");
+    let raw_contents = fs::read_to_string("input.txt").expect("File not found");
     let contents = raw_contents.trim(); // remove trailing \n
 
     let orbits = input_to_orbits(contents);
 
-    // Count the distance to COM for each individual planet.
+    // Part 1. Count the distance to COM for each individual planet.
     let num_orbits = orbits.keys().clone().fold(0, |sum, planet| {
         sum + count_to_center(orbits.clone(), *planet, 0)
     });
+    println!("Part 1: {}", num_orbits);
 
-    println!("{}", num_orbits);
+    // Part 2. Count distance until common orbital ancestor.
+    let dist_from_san = count_to_destination(orbits.clone(), vec!["YOU"], vec!["SAN"]);
+    println!("Part 2: {}", dist_from_san);
 }
 
 fn input_to_orbits(input: &str) -> HashMap<&str, &str> {
@@ -27,5 +30,24 @@ fn count_to_center(orbits: HashMap<&str, &str>, planet: &str, sum: usize) -> usi
         Some(&"COM") => sum + 1,
         Some(p) => count_to_center(orbits.clone(), p, sum + 1),
         None => unreachable!(),
+    }
+}
+
+fn count_to_destination(orbits: HashMap<&str, &str>, from: Vec<&str>, to: Vec<&str>) -> usize {
+    let f = from.iter().last().unwrap();
+    let t = to.iter().last().unwrap();
+
+    let f_next = orbits.get(f).unwrap_or(&f);
+    let t_next = orbits.get(t).unwrap_or(&t);
+
+    match (from.iter().position(|x| x == t_next), to.iter().position(|x| x == f_next)) {
+        (Some(_), Some(_)) => from.len() + to.len() - 2,
+        (Some(i), None) => i + to.len() - 2,
+        (None, Some(i)) => from.len() + i - 2,
+        (None, None) => count_to_destination(
+            orbits.clone(),
+            [from.clone(), vec![f_next]].concat(),
+            [to.clone(), vec![t_next]].concat()
+        ),
     }
 }
