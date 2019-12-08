0cfa840a3f8acac67c4af002f018b052b0c2918f
Seth Etter <mail@sethetter.com>
2019-12-07 20:57:09 -0600

    Day 7, part 2, sheeesh

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -9,16 +9,24 @@ struct IntCodeComp {
     pos: usize,
     codes: IntCodes,
     inputs: Vec<isize>,
-    output: Vec<isize>,
+    done: bool,
 }
 
 impl IntCodeComp {
     fn new(codes: IntCodes, inputs: Vec<isize>) -> IntCodeComp {
-        IntCodeComp{codes: codes, inputs: inputs, pos: 0, output: vec![]}
+        IntCodeComp{
+            codes: codes,
+            inputs: inputs,
+            pos: 0,
+            done: false,
+        }
     }
 
-    fn first_out(&self) -> isize {
-        *self.clone().skip_while(|outs| outs.len() == 0).nth(0).unwrap().iter().nth(0).unwrap()
+    fn input(&mut self, v: Option<isize>) {
+        match v {
+            None => { self.done = true; },
+            Some(x) => { self.inputs.splice(0..0, vec![x].iter().cloned()); },
+        }
     }
 
     fn param(&self, t: char, v: isize) -> isize {
@@ -39,74 +47,82 @@ impl IntCodeComp {
 }
 
 impl Iterator for IntCodeComp {
-    type Item = Vec<isize>;
+    type Item = isize;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.codes[self.pos] == 99 { return None }
+        if self.done { return None; }
+        loop {
+            if self.pos >= self.codes.len() { return None; }
+            if self.codes[self.pos] == 99 { return None; }
 
-        let opcode = format!("{:0>6}", self.codes[self.pos]);
-        match opcode.chars().nth(5) {
-            Some('1') => { // Add
-                let params = self.n_params(opcode, 2);
-                let dest = self.codes[self.pos+3] as usize;
-                self.codes[dest] = params[0] + params[1];
-                self.pos += 4;
-            },
-            Some('2') => { // Mult
-                let params = self.n_params(opcode, 2);
-                let dest = self.codes[self.pos+3] as usize;
-                self.codes[dest] = params[0] * params[1];
-                self.pos += 4;
-            },
-            Some('3') => { // Input
-                let dest = self.codes[self.pos+1] as usize;
-                self.codes[dest] = self.inputs.pop().unwrap();
-                self.pos += 2;
-            },
-            Some('4') => { // Output
-                self.output.push(*self.n_params(opcode, 1).first().unwrap());
-                self.pos += 2;
-            },
-            Some('5') => { // Jump If True
-                let params = self.n_params(opcode, 2);
-                if params[0] > 0 {
-                    self.pos = params[1] as usize;
-                } else {
-                    self.pos += 3;
-                }
-            },
-            Some('6') => { // Jump If False
-                let params = self.n_params(opcode, 2);
-                if params[0] == 0 {
-                    self.pos = params[1] as usize;
-                } else {
-                    self.pos += 3;
-                }
-            },
-            Some('7') => { // Less Than
-                let params = self.n_params(opcode, 2);
-                let dest = self.codes[self.pos+3] as usize;
-                if params[0] < params[1] {
-                    self.codes[dest] = 1;
-                } else {
-                    self.codes[dest] = 0;
-                }
-                self.pos += 4;
-            },
-            Some('8') => { // Equals
-                let params = self.n_params(opcode, 2);
-                let dest = self.codes[self.pos+3] as usize;
-                if params[0] == params[1] {
-                    self.codes[dest] = 1;
-                } else {
-                    self.codes[dest] = 0;
-                }
-                self.pos += 4;
-            },
-            Some(_) => return None,
-            None => return None,
+            let opcode = format!("{:0>6}", self.codes[self.pos]);
+            match opcode.chars().nth(5) {
+                Some('1') => { // Add
+                    let params = self.n_params(opcode, 2);
+                    let dest = self.codes[self.pos+3] as usize;
+                    self.codes[dest] = params[0] + params[1];
+                    self.pos += 4;
+                },
+                Some('2') => { // Mult
+                    let params = self.n_params(opcode, 2);
+                    let dest = self.codes[self.pos+3] as usize;
+                    self.codes[dest] = params[0] * params[1];
+                    self.pos += 4;
+                },
+                Some('3') => { // Input
+                    let dest = self.codes[self.pos+1] as usize;
+                    let v = match self.inputs.pop() {
+                        Some(i) => i,
+                        None => return None,
+                    };
+                    self.codes[dest] = v;
+                    self.pos += 2;
+                },
+                Some('4') => { // Output
+                    let x = *self.n_params(opcode, 1).first().unwrap();
+                    self.pos += 2;
+                    return Some(x);
+                },
+                Some('5') => { // Jump If True
+                    let params = self.n_params(opcode, 2);
+                    if params[0] > 0 {
+                        self.pos = params[1] as usize;
+                    } else {
+                        self.pos += 3;
+                    }
+                },
+                Some('6') => { // Jump If False
+                    let params = self.n_params(opcode, 2);
+                    if params[0] == 0 {
+                        self.pos = params[1] as usize;
+                    } else {
+                        self.pos += 3;
+                    }
+                },
+                Some('7') => { // Less Than
+                    let params = self.n_params(opcode, 2);
+                    let dest = self.codes[self.pos+3] as usize;
+                    if params[0] < params[1] {
+                        self.codes[dest] = 1;
+                    } else {
+                        self.codes[dest] = 0;
+                    }
+                    self.pos += 4;
+                },
+                Some('8') => { // Equals
+                    let params = self.n_params(opcode, 2);
+                    let dest = self.codes[self.pos+3] as usize;
+                    if params[0] == params[1] {
+                        self.codes[dest] = 1;
+                    } else {
+                        self.codes[dest] = 0;
+                    }
+                    self.pos += 4;
+                },
+                Some(_) => return None,
+                None => return None,
+            }
         }
-        Some(self.output.clone())
     }
 }
 
@@ -116,14 +132,28 @@ fn main() {
 
     let intcodes: IntCodes = contents.split(',').map(|x| x.parse().unwrap()).collect();
 
-    let max_out_perm = permutations(vec![0,1,2,3,4]).iter().fold(0, |prev_max, perms| {
-        let amp1 = IntCodeComp::new(intcodes.clone(), vec![0, perms[0]]);
-        let amp2 = IntCodeComp::new(intcodes.clone(), vec![amp1.first_out(), perms[1]]);
-        let amp3 = IntCodeComp::new(intcodes.clone(), vec![amp2.first_out(), perms[2]]);
-        let amp4 = IntCodeComp::new(intcodes.clone(), vec![amp3.first_out(), perms[3]]);
-        let amp5 = IntCodeComp::new(intcodes.clone(), vec![amp4.first_out(), perms[4]]);
+    let max_out_perm = permutations(vec![5,6,7,8,9]).iter().fold(0, |prev_max, perms| {
+        let mut amp1 = IntCodeComp::new(intcodes.clone(), vec![perms[0]]);
+        let mut amp2 = IntCodeComp::new(intcodes.clone(), vec![perms[1]]);
+        let mut amp3 = IntCodeComp::new(intcodes.clone(), vec![perms[2]]);
+        let mut amp4 = IntCodeComp::new(intcodes.clone(), vec![perms[3]]);
+        let mut amp5 = IntCodeComp::new(intcodes.clone(), vec![perms[4]]);
 
-        max(amp5.first_out(), prev_max)
+        let mut input: Option<isize> = Some(0);
+
+        loop {
+            amp1.input(input);
+            amp2.input(amp1.next());
+            amp3.input(amp2.next());
+            amp4.input(amp3.next());
+            amp5.input(amp4.next());
+            match amp5.next() {
+                Some(x) => input = Some(x),
+                None => break,
+            }
+        }
+
+       max(prev_max, input.unwrap())
     });
 
     println!("Part1 : {}", max_out_perm);
