afad923bd931b35750812cb61a27764f31747537
Seth Etter <mail@sethetter.com>
2019-12-08 10:39:50 -0600

    Day 8, part 2

--- day-08/src/main.rs
+++ day-08/src/main.rs
@@ -4,12 +4,16 @@ fn main() {
     let raw_input = fs::read_to_string("input.txt").expect("Failed to read input file.");
     let input = raw_input.trim(); // get rid of tailing \n
 
-    let w = 25;
-    let h = 6;
+    const W: usize = 25;
+    const H: usize = 6;
+    let size = W * H;
 
     let nums: Vec<u32> = input.chars().map(|c| c.to_digit(10).unwrap()).collect();
+    let layers = nums.chunks(size);
 
-    let layer_w_fewest_zeros = nums.chunks(w * h).min_by(|f1, f2| {
+    // Part 1
+
+    let layer_w_fewest_zeros = layers.clone().min_by(|f1, f2| {
         let f1_0s = f1.iter().filter(|x| *x == &0).count();
         let f2_0s = f2.iter().filter(|x| *x == &0).count();
         f1_0s.cmp(&f2_0s)
@@ -19,4 +23,18 @@ fn main() {
     let count_2s = layer_w_fewest_zeros.iter().filter(|x| *x == &2).count();
 
     println!("Part 1: {}", count_1s * count_2s);
-}
+
+    // Part 2
+    let final_img: Vec<u32> = layers.clone().fold(vec![2; size], |img, l| {
+        img.iter().zip(l.iter()).map(|(ip, lp)| match ip {
+            2 => *lp,
+            _ => *ip,
+        }).collect()
+    });
+
+    println!("Part 2: {:?}", final_img);
+
+    // Challenge was completed by taking the output from the println, pasting it
+    // into vim, stripping it down to the numbers, splitting it into rows, and
+    // replacing all the 0's with spaces. This showed ZFLBY.
+ }
